5c9cee55a986992f89b3d7ef39866976e6d5b069
Sudip Das <sudip5428@gmail.com>
2022-08-29 20:54:51 +0530

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 3001;
 app.use(express.json());
 // app.use(cors());
 app.get("/", (req, res) => {
